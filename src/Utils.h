6544b858382d08da0664a899e3020c3e84b38a94
geugenio <geugnio12@gmail.com>
2025-07-15 23:09:16 -0300

    feat(Utils): classe utilitária adicionada

---
+++ src/Utils.h
@@ -0,0 +1,10 @@
+/*
+Nesse util, vai ficar as funções:
+    lerNum, lerNumIntervalo, lerString
+    funções de exibição de menus
+    inicilização de dados
+
+Isso vai ser de bastante utilidade pra enxugar o nosso main, que tá muito grande
+
+*/
+
